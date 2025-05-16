aaea38b60d05760ef5a5b6bf178533d5a3a51627
ReddieHal <grodri87@calpoly.edu>
2025-05-15 18:09:13 -0700

    in-progress

--- mapreduce/tracker.go
+++ mapreduce/tracker.go
@@ -1,10 +1,13 @@
 package mapreduce
 
-import ("sync"
-
+import (
+	"sync"
+	"time"
+	"errors"
 )
 
 type TaskPhase int
+
 const (
 	MapPhase TaskPhase = iota
 	ReducePhase
@@ -12,34 +15,94 @@ const (
 )
 
 type TaskStatus int
+
 const (
 	Idle TaskStatus = iota
 	InProgress
 	Completed
 )
 
-type MasterTask struct {
-	files []string // list we're processing
-	nReducer int
-	mutex sync.Mutex
-	phase TaskPhase
-	status TaskStatus
-	mapTasks []TaskStatus
-	reduceTasks []TaskStatus
+type GetTaskArgs struct{}
 
-	cond *sync.Cond
+type GetTaskReply struct {
+	TaskType     TaskPhase
+	FileName     string
+	MapTaskID    int
+	ReduceTaskID int
+	NReduce      int
 }
 
-func MakeMaster(files []string, nReducer int) *MasterTask {
+type ReportTaskArgs struct {
+	TaskType TaskPhase
+	TaskID   int
+}
+type ReportTaskReply struct{}
+
+type MasterTask struct {
+	files   []string
+	nReduce int // number of reduce taks
+
+	mu   sync.Mutex
+	cond *sync.Cond
+
+	phase           TaskPhase
+	mapStatus       []TaskStatus
+	reduceStatus    []TaskStatus
+	mapStartTime    []time.Time
+	reduceStartTime []time.Time
+}
+
+func MakeMaster(files []string, nReduce int) *MasterTask {
 	m := &MasterTask{
-		files: files,
-		nReducer: nReducer, // number of reducers
-		phase: MapPhase,
-		status: Idle,
-		mapTasks: make([]TaskStatus, len(files)),
-		reduceTasks: make([]TaskStatus, nReducer),
+		files:           files,
+		nReduce:         nReduce,
+		phase:           MapPhase,
+		mapStatus:       make([]TaskStatus, len(files)),
+		reduceStatus:    make([]TaskStatus, nReduce),
+		mapStartTime:    make([]time.Time, len(files)),
+		reduceStartTime: make([]time.Time, nReduce),
 	}
-	m.cond = sync.NewCond(&m.mutex)
-	
+	m.cond = sync.NewCond(&m.mu)
 	return m
+}
+
+func (m *MasterTask) GetTask(args GetTaskArgs, reply *GetTaskReply) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.phase == DonePhase {
+		return errors.New("all tasks are done")
+	}
+
+	for i, status := range m.mapStatus {
+		if status == Idle ||
+			(status == InProgress && time.Since(m.mapStartTime[i]) > 10*time.Second) {
+			m.mapStatus[i] = InProgress
+			m.mapStartTime[i] = time.Now()
+
+			*reply = GetTaskReply{
+				TaskType:    MapPhase,
+				FileName:    m.files[i],
+				MapTaskID:   i,
+				NReduce: m.nReduce,
+			}
+			return nil
+		}
+	
+	}
+	allDone := true
+
+	for _, status := range m.mapStatus {
+		if status != Completed {
+			allDone = false
+		}
+	}
+	if allDone && m.phase == MapPhase {
+		m.phase = ReducePhase
+	}
+	
+	if m.phase == DonePhase {
+		*reply = GetTask
+	}
+
 }
